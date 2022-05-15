cabbe8f668d18a130436e2b5991f098aa33a6bf1
Caio Lamarão <caiolamarao@outlook.com>
2022-05-15 09:13:44 -0300

    Pagination

--- frontend/src/components/MovieScore/index.tsx
+++ frontend/src/components/MovieScore/index.tsx
@@ -14,7 +14,7 @@ function MovieScore({ score , count } : Props) {
     return (
         <div className="dsmovie-score-container">
             <p className="dsmovie-score-value">{score > 0 ? score.toFixed(1) : '-'}</p>
-            <MovieStars score={score}/>
+            <MovieStars score={score} />
             <p className="dsmovie-score-count">{count} avaliações</p>
         </div>
     );
